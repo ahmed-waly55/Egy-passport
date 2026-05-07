afda1fabd224a643616546ad56052c627f84b0e8
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-07 18:50:07 +0300

    added custom component Btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BtnComponent } from './shared/components/btn/btn.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BtnComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/components/btn/btn.component.ts
@@ -0,0 +1,19 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-btn',
+  imports: [NgStyle,RouterLink],
+  templateUrl: './btn.component.html',
+  styleUrl: './btn.component.css'
+})
+export class BtnComponent {
+ @Input() label: string = 'تسجيل الدخول'; // default label in Arabic
+  @Input() customColor: string = '#b3000e'; // default color (a shade of red)
+  @Input() iconClass: string = 'bi bi-arrow-left'; // default icon (Bootstrap Icons)
+
+  @Input() width: string = '100%';  // default to full width
+  @Input() route: string = '';  // optional route for navigation
+  @Input() isDisabled: boolean = false; // to control button state
+}
