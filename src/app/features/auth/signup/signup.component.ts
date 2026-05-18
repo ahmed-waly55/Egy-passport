12ec7a24a3f5152bf582f7d40dcf9d92c1d3fd73
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-18 13:46:19 +0300

    add signup page

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,44 +1,79 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FieldComponent } from '../../../shared/components/field/field.component';
 import { BtnComponent } from "../../../shared/components/btn/btn.component";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LangComponent } from "../../../shared/components/lang/lang.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [FieldComponent, BtnComponent,ReactiveFormsModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FieldComponent,
+    BtnComponent,
+    LangComponent,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
-   SignUpForms = new FormGroup({
-    emailOrPhone: new FormControl('', [
+  activeLang = { code: 'ar', name: 'العربية', dir: 'rtl' };
+
+  signupForm = new FormGroup({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    nationalId: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^([1-9]{1})([0-9]{13})$/)
+    ]),
+    email: new FormControl('', [
       Validators.required,
       Validators.email
     ]),
-
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^01[0125][0-9]{8}$/)
+    ]),
     password: new FormControl('', [
       Validators.required,
-      Validators.minLength(6),
+      Validators.minLength(6)
     ]),
+    confirmPassword: new FormControl('', [
+      Validators.required
+    ]),
+    agreeToTerms: new FormControl(false, [
+      Validators.requiredTrue
+    ])
+  }, {
+
+    validators: (group) => {
+      const pass = group.get('password')?.value;
+      const confirmPass = group.get('confirmPassword')?.value;
+      return pass === confirmPass ? null : { notSame: true };
+    }
   });
 
-  get emailOrPhone() {
-    return this.SignUpForms.controls.emailOrPhone;
-  }
-
-  get password() {
-    return this.SignUpForms.controls.password;
-  }
+  get fullName() { return this.signupForm.controls.fullName; }
+  get nationalId() { return this.signupForm.controls.nationalId; }
+  get email() { return this.signupForm.controls.email; }
+  get phone() { return this.signupForm.controls.phone; }
+  get password() { return this.signupForm.controls.password; }
+  get confirmPassword() { return this.signupForm.controls.confirmPassword; }
+  get agreeToTerms() { return this.signupForm.controls.agreeToTerms; }
 
   onSubmit() {
-    if (this.SignUpForms.invalid) {
-      this.SignUpForms.markAllAsTouched();
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
       return;
     }
 
-    console.log(this.SignUpForms.value);
+    console.log('بيانات الحساب الجديد:', this.signupForm.value);
   }
-
-
 }
