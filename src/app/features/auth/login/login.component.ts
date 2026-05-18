0ec0dd7d6d1badc4520260069217c05125e3fa14
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-18 13:04:44 +0300

    add lang rusable component

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BtnComponent } from "../../../shared/components/btn/btn.component";
+import { FieldComponent } from "../../../shared/components/field/field.component";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { LangComponent } from "../../../shared/components/lang/lang.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [BtnComponent, FieldComponent, ReactiveFormsModule, CommonModule, RouterLink, LangComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    emailOrPhone: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  get emailOrPhone() {
+    return this.loginForm.controls.emailOrPhone;
+  }
+
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
+}
+
+
+
+
+
