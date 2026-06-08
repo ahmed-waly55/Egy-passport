6ebea9f9c6c529ed81e43b698564deac1ae28dcb
esraa-kaf <esraakaf3@gmail.com>
2026-06-08 22:10:47 +0300

    make integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes ,
-     withViewTransitions(),
-     withInMemoryScrolling({scrollPositionRestoration: 'top'})),
-      provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions(), withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+  ],
 };
